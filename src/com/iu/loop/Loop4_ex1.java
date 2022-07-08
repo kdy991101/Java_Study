960b2c962ff2a713356688915ce8eb4438ff8d7c
kdy991101 <kdy991101@naver.com>
2022-07-08 10:41:54 +0900

    78

--- src/com/iu/loop/Loop4_ex1.java
+++ src/com/iu/loop/Loop4_ex1.java
@@ -28,7 +28,35 @@ public class Loop4_ex1 {
 			}//2포
 			
 		}//1포
+		
+	
+		     
+		         System.out.println("분을 입력해주세요");
+		         int m1 = sc.nextInt();
+		         System.out.println("초를 입력해주세요");
+		         int s1 = sc.nextInt();      
+		         int m2 = 0;
+		         int s2 = 0;
+		         
+		         boolean flag = false;
+		         
+		         for(int m = 0; m < 60; m++) {
+		            
+		            for(int s = 0; s < 60; s++) {
+		               System.out.println(m + "분 :" + s + "초" );
+		               if(m1 == m && s1 == s) {
+		                  flag = !flag;
+		                  m = 60;
+		                  break;
+		               }
+		            }
+		            if(flag) {
+		                  break;
+		            }
+		         }
+		   }
+		
 
 	}
 
-}
+
---
+++ src/com/iu/loop/Loop4_ex2.java
@@ -0,0 +1,104 @@
+package com.iu.loop;
+
+import java.util.Scanner;
+
+public class Loop4_ex2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		FPS
+//		총알 : 30발, 3개
+		
+//		1. 단발 2. 점사
+//    소리 탕
+			Scanner sc = new Scanner(System.in);
+			for(int q = 0; q < 3; q++) {
+			System.out.println("1번\'단발\'  2번\'점사\' 선택하세요");
+			
+			 int num = sc.nextInt();	 
+			String tang = "탕";
+			String tang2 = "탕탕탕\t";
+			
+				if(num == 1) {
+					
+					for(int i = 0; i <= 30; i++) 
+						System.out.print(tang);
+					} else {
+						for(int j = 0; j <= 10; j++) 
+							System.out.print(tang2);
+						
+					}
+			}
+//	==================================================================강사님 풀이1.
+//						Scanner sc = new Scanner(System.in);
+//						for(int mag = 0; mag < 3; mag++) {
+						
+//						System.out.println("1. 단발 2.점사");
+//						int select = sc.nextInt();
+//						if(select == 1) {
+//							fot(int b = 0; b < 30; b++)
+//						System.out.println("탄창 : " + mag + " 총알:" + b + "탕");
+//							
+//						}else {
+//							fot(int b = 0; b < 10; b++)
+//						System.out.println("탄창 : " + mag + " 총알:" + b + "탕탕탕");
+//							
+//						}
+//===================================================================================2.
+//			Scanner sc = new Scanner(System.in);	 
+//			for(int mag = 0; mag < 3; mag++) {
+//				System.out.println("1단발 2점사");
+//				 int select = sc.nextInt();
+//				 int count = 10;
+//				 String sound = "탕탕탕";
+//				 if(select == 1) {
+//					 count = 30;
+//					 sound = "탕";
+//				 }
+//				for (int b = 0; b<count; b++) {
+//					System.out.println(sound);
+//			}
+//						
+//					
+//		}
+//	}
+//====================================================================================3.
+//			Scanner sc = new Scanner(System.in);	 
+//			for(int mag = 0; mag < 3; mag++) {
+//				System.out.println("1단발 2점사");
+//				 int select = sc.nextInt();
+//				int inc = 3;
+//				String sound = "탕탕탕";
+//				if(select == 1) {
+//					inc = 1;
+//					sound = "탕";
+//				}
+//				for(int b = 0; b<3; b+=inc) {
+//					System.out.println(sound);
+//				}
+//======================================================================================			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	}			
+
+}
