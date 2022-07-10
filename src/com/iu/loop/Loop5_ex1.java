4310a94f71c4515b81ec0ea0609279a5798899ea
kdy991101 <kdy991101@naver.com>
2022-07-11 08:39:11 +0900

    711loop

--- src/com/iu/loop/Loop5_ex1.java
+++ src/com/iu/loop/Loop5_ex1.java
@@ -27,55 +27,51 @@ public class Loop5_ex1 {
 		int ypw = 12;
 
 		boolean ch = true;
-		
-		
-		while(ch) {
+
+		while (ch) {
 			System.out.println("게임을 시작하려면 1을 누르세요.");
 			int num2 = sc.nextInt();
-				if(num2 == 1) 
-			System.out.println("1. 로그인 2. 뒤로가기");
+			if (num2 == 1)
+				System.out.println("1. 로그인 2. 뒤로가기");
 			int num = sc.nextInt();
-				
+
 			int q = 1;
 			int w = 2;
 			int won = 1000;
 			int nuu = 0;
-				
-			if(num == q) {
+
+			if (num == q) {
 				System.out.println("ID입력하세요");
 				int p = sc.nextInt();
-				
+
 				System.out.println("PW입력");
 				int m = sc.nextInt();
-				
-				if(p == yid && m == ypw ) {
+
+				if (p == yid && m == ypw) {
 					System.out.println("로그인 성공");
-					
-					for(int i = 1; i <= 15; i++) {
+
+					for (int i = 1; i <= 15; i++) {
 						System.out.println("랩업을 축하합니다! " + i + "랩 입니다.");
-						
-						for(int exp = 1; exp<=3*i; exp++) {
-								nuu++;
-								System.out.println(exp  +"몬스터를 처치하였습니다.");
-								System.out.println("누적 처치 몬스터 :" +  nuu );
-								
-							if (i%5 == 0) {
-								System.out.println("골드:" + ((i/5)*1000));
-								won += ((i/5)*1000);
+
+						for (int exp = 1; exp <= 3 * i; exp++) {
+							nuu++;
+							System.out.println(exp + "몬스터를 처치하였습니다.");
+							System.out.println("누적 처치 몬스터 :" + nuu);
+
+							if (i % 5 == 0) {
+								System.out.println("골드:" + ((i / 5) * 1000));
+								won += ((i / 5) * 1000);
 								break;
-								}
-							nuu = exp;
-								}
 							}
-					System.out.println("보유 골드:" + won + "원" );
+							nuu = exp;
 						}
-					break;
-				}else {
-					System.out.println("실패");
+					}
+					System.out.println("보유 골드:" + won + "원");
 				}
-			}		
+				break;
+			} else {
+				System.out.println("실패");
+			}
 		}
 	}
-	
-	
-	
+}
