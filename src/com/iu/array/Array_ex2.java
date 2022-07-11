41dc3a793dce1d5d2b7068c49dbbd9af4be678ec
kdy991101 <kdy991101@naver.com>
2022-07-11 17:46:24 +0900

    배열 와일 몬스터

--- src/com/iu/array/Array_ex2.java
+++ src/com/iu/array/Array_ex2.java
@@ -13,9 +13,18 @@ public class Array_ex2 {
 		String[] monNames = {"고블린", "오크", "트롤", "골렘"};
 		int [] monHp = {5, 7, 10, 15};
 		int count;
+		
+//		(마법사)캐릭터 이름 설정
+		System.out.println("캐릭터 이름 생성");
+		String name = sc.next();
+		System.out.println("회원님의 닉네임은 \'" + name + "\'입니다.");
+		
+//		스킬 파이어볼
+//		공격력 6
+		
 //		몬스터 파티 생성
 //		몬스터 수 입력 1~4
-//		입력한 수만큼 몬스터를 랜덤라게 뽑고 출력
+//		입력한 수만큼 몬스터를 랜덤하게 뽑고 출력
 		System.out.println("몬스터 수를 입력하세요");
 		count = sc.nextInt();
 		System.out.println("입력하신 몬스터 수 :" + count);
@@ -28,10 +37,42 @@ public class Array_ex2 {
 			monPartyNames[i] = monNames[index];
 			monPartyHp[i] = monHp[index];
 		}
+//		1회성 공격
+//		0~6미만의 랜덤한 데미지로 공격
+		boolean check = true;
+		
+		int ok;
+		
 		for(int i = 0; i<monPartyNames.length; i++)
 		{
-			System.out.println("몬스터 이름 :" + monPartyNames[i] + "\nHP :" + monPartyHp[i]);
+			System.out.println("몬스터 이름 :\'" + monPartyNames[i] + "\'\nHP :" + monPartyHp[i]);
+		}
+		while(check)
+		{
+			int death = 0;
+		System.out.println("공격 하시겠습니까?");
+		ok = sc.nextInt();
+		if(ok == 1)
+		{
+			//int q;
+			for(int i = 0; i<monPartyNames.length; i++) 
+			{
+				int magicPower = random.nextInt(6);
+			System.out.println("\'" + monPartyNames[i] + "\'을(를) 공격합니다");
+			monPartyHp[i] -= magicPower;
+			System.out.println("데미지" + magicPower +"만큼 들어갔습니다\n" + "몬스터의 남은 HP :" + monPartyHp[i] + "입니다.");
+			if(monPartyHp[i] < 1)
+			{
+				death++;
+				System.out.println("처치한 수" + death);
+			}
 		}
 	}
+			
+				
+			}
+			
+		
+	}//main
 
-}
+}//class
